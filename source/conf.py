78315c989b8a585a84d395c7eeb6b98d03f33735
Russ Berg <russ.berg@lightsource.ca>
2025-04-02 11:46:20 -0600

    renamed templates to _templates

--- source/conf.py
+++ source/conf.py
@@ -72,8 +72,8 @@ source_suffix = {
 }
 
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+# Add any paths that contain _templates here, relative to this directory.
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
